15fdf2abc021835a2bc1a6852590dbff2225a8ef
Alex Mariano <amarianocello@gmail.com>
2022-07-01 21:11:14 -0700

    Added Save Ability in DataManager.cs
    Added Save Ability in DataManager.cs

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DataManager : MonoBehaviour
+{
+
+    public static DataManager Instance;
+
+    public string username;
+    public int highScore;
+
+	private void Awake() {
+		if (Instance != null) {
+			Destroy(gameObject);
+			return;
+		}
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	[System.Serializable]
+	class SaveData {
+		public int highScore;
+	}
+
+	public void saveHighScore() {
+		SaveData data = new SaveData();
+		data.highScore = highScore;
+		string json = JsonUtility.ToJson(data);
+		File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+	}
+
+	public void loadHighScore() {
+		string path = Application.persistentDataPath + "/savefile.json";
+		if (File.Exists(path)) {
+			string json = File.ReadAllText(path);
+			SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+			highScore = data.highScore;
+		}
+	}
+}
