6e34389eb71deaede4935f427affa834f2dc6dec
Alex Mariano <amarianocello@gmail.com>
2022-07-01 22:00:38 -0700

    Added text display code for persistent data

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -22,12 +22,14 @@ public class DataManager : MonoBehaviour
 
 	[System.Serializable]
 	class SaveData {
+		public string username;
 		public int highScore;
 	}
 
 	public void saveHighScore() {
 		SaveData data = new SaveData();
 		data.highScore = highScore;
+		data.username = username;
 		string json = JsonUtility.ToJson(data);
 		File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
 	}
@@ -37,7 +39,7 @@ public class DataManager : MonoBehaviour
 		if (File.Exists(path)) {
 			string json = File.ReadAllText(path);
 			SaveData data = JsonUtility.FromJson<SaveData>(json);
-
+			username = data.username;
 			highScore = data.highScore;
 		}
 	}
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuUIHandler : MonoBehaviour
 {
+
+    public InputField usernameInput;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -18,6 +22,8 @@ public class MenuUIHandler : MonoBehaviour
     }
 
     public void StartNew() {
+        string username = usernameInput.text;
+        DataManager.Instance.username = username;
         SceneManager.LoadScene(1);
 	}
 }
